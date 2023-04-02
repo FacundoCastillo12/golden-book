aaa18f45da1f1105bffb2e483398c0cc2b6fa697
FacundoCastillo12 <facuelfacha15@gmail.com>
2023-04-02 20:45:37 -0300

    Add main component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "@/styles/globals.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { AppProps } from "next/app";
+import LanguageContextProvider, {
+  LanguageContext,
+} from "@/contexts/ContextLanguage";
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LanguageContextProvider>
+      <LanguageContext.Consumer>
+        {() => <Component {...pageProps} />}
+      </LanguageContext.Consumer>
+    </LanguageContextProvider>
+  );
 }
